6bbd2666a73871ff04b530b8590c6eaf6183956a
phpg21 <ppalaciosgoez@gmail.com>
2024-10-17 10:31:41 -0500

    validar_longitud

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,11 @@
 # TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+
+class ReglaValidacion:
+    def __init__(self, longitud_esperada):
+        self._longitud_esperada = longitud_esperada
+
+    def validar_longitud(self, clave):
+        return len(clave) > self._longitud_esperada
+
