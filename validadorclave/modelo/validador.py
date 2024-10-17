8839509e643740ce290b4125a5a67757799d07ef
phpg21 <ppalaciosgoez@gmail.com>
2024-10-17 10:36:15 -0500

    _contiene_numero

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -3,9 +3,18 @@ from abc import ABC, abstractmethod
 
 
 class ReglaValidacion:
-    def __init__(self, longitud_esperada):
-        self._longitud_esperada = longitud_esperada
+    def __init__(self, _longitud_esperada):
+        self._longitud_esperada = _longitud_esperada
 
     def _validar_longitud(self, clave):
         return len(clave) > self._longitud_esperada
+
+    def _contiene_mayuscula(self, clave):
+        return any(char.isupper() for char in clave)
+
+    def _contiene_minuscula(self, clave):
+        return any(char.islower() for char in clave)
+
+    def _contiene_numero(self, clave):
+        return any(char.isdigit() for char in clave)
 
